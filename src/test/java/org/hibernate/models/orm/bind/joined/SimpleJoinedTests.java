b10be715b79060fc8618565ea86a85dc9b97a8ce
Steve Ebersole <steve@hibernate.org>
2026-05-21 15:39:48 -0600

    Completed composite-id testing

--- src/test/java/org/hibernate/models/orm/bind/joined/SimpleJoinedTests.java
+++ src/test/java/org/hibernate/models/orm/bind/joined/SimpleJoinedTests.java
@@ -14,8 +14,11 @@ import org.junit.jupiter.api.Test;
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.DiscriminatorType;
 import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
@@ -69,6 +72,66 @@ public class SimpleJoinedTests {
 		);
 	}
 
+	@SuppressWarnings("JUnitMalformedDeclaration")
+	@Test
+	@ServiceRegistry
+	void compositeIdTest(ServiceRegistryScope scope) {
+		checkDomainModel(
+				(context) -> {
+					final var metadataCollector = context.getMetadataCollector();
+					final RootClass rootBinding = (RootClass) metadataCollector.getEntityBinding( CompositeRoot.class.getName() );
+					final JoinedSubclass subBinding = (JoinedSubclass) metadataCollector.getEntityBinding( CompositeSub.class.getName() );
+
+					assertThat( rootBinding.getTable().getPrimaryKey() ).isNotNull();
+					assertThat( rootBinding.getTable().getPrimaryKey().getColumns() )
+							.extracting( org.hibernate.mapping.Column::getName )
+							.containsExactly( "id1", "id2" );
+
+					assertThat( subBinding.getKey() ).isNotNull();
+					assertThat( subBinding.getKey().getColumns() )
+							.extracting( org.hibernate.mapping.Column::getName )
+							.containsExactly( "id1", "id2" );
+					assertThat( subBinding.getTable().getPrimaryKey() ).isNotNull();
+					assertThat( subBinding.getTable().getPrimaryKey().getColumns() )
+							.extracting( org.hibernate.mapping.Column::getName )
+							.containsExactly( "id1", "id2" );
+				},
+				scope.getRegistry(),
+				CompositeRoot.class,
+				CompositeSub.class
+		);
+	}
+
+	@SuppressWarnings("JUnitMalformedDeclaration")
+	@Test
+	@ServiceRegistry
+	void idClassTest(ServiceRegistryScope scope) {
+		checkDomainModel(
+				(context) -> {
+					final var metadataCollector = context.getMetadataCollector();
+					final RootClass rootBinding = (RootClass) metadataCollector.getEntityBinding( IdClassRoot.class.getName() );
+					final JoinedSubclass subBinding = (JoinedSubclass) metadataCollector.getEntityBinding( IdClassSub.class.getName() );
+
+					assertThat( rootBinding.getTable().getPrimaryKey() ).isNotNull();
+					assertThat( rootBinding.getTable().getPrimaryKey().getColumns() )
+							.extracting( org.hibernate.mapping.Column::getName )
+							.containsExactly( "id1", "id2" );
+
+					assertThat( subBinding.getKey() ).isNotNull();
+					assertThat( subBinding.getKey().getColumns() )
+							.extracting( org.hibernate.mapping.Column::getName )
+							.containsExactly( "id1", "id2" );
+					assertThat( subBinding.getTable().getPrimaryKey() ).isNotNull();
+					assertThat( subBinding.getTable().getPrimaryKey().getColumns() )
+							.extracting( org.hibernate.mapping.Column::getName )
+							.containsExactly( "id1", "id2" );
+				},
+				scope.getRegistry(),
+				IdClassRoot.class,
+				IdClassSub.class
+		);
+	}
+
 	@Entity(name="Root")
 	@Table(name="Root")
 	@Inheritance(strategy = InheritanceType.JOINED)
@@ -87,5 +150,49 @@ public class SimpleJoinedTests {
 		@Id
 		private Integer id;
 		private String name;
+	}
+
+	@Entity(name="CompositeRoot")
+	@Table(name="CompositeRoot")
+	@Inheritance(strategy = InheritanceType.JOINED)
+	public static class CompositeRoot {
+		@EmbeddedId
+		private Pk id;
+		private String name;
+	}
+
+	@Entity(name="CompositeSub")
+	@Table(name="CompositeSub")
+	public static class CompositeSub extends CompositeRoot {
+		private String details;
+	}
+
+	@Embeddable
+	public static class Pk {
+		private Integer id1;
+		private Integer id2;
+	}
+
+	@Entity(name="IdClassRoot")
+	@Table(name="IdClassRoot")
+	@Inheritance(strategy = InheritanceType.JOINED)
+	@IdClass( IdClassPk.class )
+	public static class IdClassRoot {
+		@Id
+		private Integer id1;
+		@Id
+		private Integer id2;
+		private String name;
+	}
+
+	@Entity(name="IdClassSub")
+	@Table(name="IdClassSub")
+	public static class IdClassSub extends IdClassRoot {
+		private String details;
+	}
+
+	public static class IdClassPk {
+		private Integer id1;
+		private Integer id2;
 	}
 }
